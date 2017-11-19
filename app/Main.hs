f55b8f4a7e4d39110684151926e7ecd783da667b
Markus Krabbe Larsen <markus@krabbelarsen.dk>
2017-11-19 23:22:00 +0100

    Send a quick add request to todoist when a message is recieved

--- app/Main.hs
+++ app/Main.hs
@@ -7,27 +7,54 @@ import qualified Network.Wai as W
 import Network.HTTP.Types.Status (status200)
 import Data.Aeson ((.:))
 import qualified Data.Aeson as A
+import Network.HTTP.Simple (httpLBS, setRequestBodyURLEncoded, parseRequest, Response)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as L
+import Data.String (fromString)
+import System.Environment (lookupEnv)
 
 
 main :: IO ()
 main = do
   putStrLn "Starting server..."
-  Warp.runEnv 8000 app
+  token <- lookupEnv "TOKEN"
+  case token of
+    Just t -> Warp.runEnv 8000 (app $ fromString t)
+    Nothing -> putStrLn "Please set the TOKEN environment variable"
 
 
-app :: W.Application
-app req res = do
+app :: Token -> W.Application
+app token req res = do
   body <- W.strictRequestBody req
   let mess = (A.decode body) :: Maybe Message
   print mess
+  case mess of
+    Just (Message sender text) -> do
+      quickAdd token text
+      return ()
+    Nothing ->
+      return ()
   res $ W.responseLBS status200 [] mempty
 
 
+quickAdd :: Token -> ByteString -> IO (Response L.ByteString)
+quickAdd token text = do
+  req <- parseRequest "https://todoist.com/api/v7/quick/add"
+  let req2 = setRequestBodyURLEncoded [("token", token), ("text", text)] req
+  httpLBS req2
+
+
 data Message = Message
-  { sender :: String
-  , text :: String
+  { sender :: ByteString
+  , text :: ByteString
   } deriving (Show)
 
+
+type Token = ByteString
+
+
+instance A.FromJSON ByteString where
+  parseJSON v = fromString <$> A.parseJSON v
 
 instance A.FromJSON Message where
   parseJSON = A.withObject "Payload" $ \o -> do
